74fe2ae6d67220ec17e02e452db54bece68cd3eb
ilya <i.zhdanovich@softteco.com>
2021-11-29 15:35:47 +0300

    update

--- push.sh
+++ push.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-echo "push"
+branch_name=$(echo $BRANCH | tr '/' ' ' | awk '{ print $2 }')
+
+sudo docker login -u $login -p $passwd
+
+sudo docker build -t $login/js_app:${branch_name} .
